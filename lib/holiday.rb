5ca2b2c5db87f061310f1567a517d20a1396dd08
Craig Storer <craigstorer@gmail.com>
2020-01-07 01:55:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,7 +84,7 @@ def all_holidays_with_bbq(holiday_hash)
  
     holiday_hash.map do |season,holidays|
       holidays.map do|holiday,supply|
-       if supply include?("BBQ")
+       if supply.include?("BBQ")
         holiday
 end
 end
