d9a66d3a0093f2e97886d7877af11c39c0a948be
Craig Storer <craigstorer@gmail.com>
2020-01-07 01:05:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,6 +84,7 @@ def all_holidays_with_bbq(holiday_hash)
  
     holiday_hash.each do |season,holidays|
       holidays.each do|holiday,supply|
+      binding.pry
        if supply == "BBQ"
          supply.each do |cookout|
 end
