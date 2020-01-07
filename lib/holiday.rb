4c6ca21c982cedb9bb6a04bfbc8a05fd029c7d88
Craig Storer <craigstorer@gmail.com>
2020-01-07 00:45:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,6 +82,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
+    all_holidays_with_bbq.each do |x,|
 end
 
 
